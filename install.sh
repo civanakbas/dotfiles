13edaae1795e4806bad5f056386a443aeae2414e
civanakbas <civan.akbas@hotmail.com>
2021-09-10 15:22:23 +0300

    fixed install

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ echo Installing neovim...
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt install neovim
 
-
+echo Cloning tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo Creating symlinks...
 mkdir ~/.config/nvim
 ln -s ~/.dotfiles/nvim ~/.config/nvim
+rm ~/.gitconfig
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+rm ~/.bashrc
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
