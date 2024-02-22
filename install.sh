71065562c14e64818b33f75efa5592f4f6c883b3
civanakbas <civannakbas@gmail.com>
2024-02-22 16:35:33 +0300

    cleanup

--- install.sh
+++ install.sh
@@ -3,26 +3,13 @@ pacman -S tmux mpv xsel npm python-pip ripgrep fd lsd neovim
 
 pip install pynvim
 
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
 echo Cloning tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 echo Creating symlinks...
 
-rm -rf ~/.config/lvim
-ln -s ~/.dotfiles/lvim/ ~/.config
-rm ~/.gitconfig
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-rm ~/.bashrc
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
-rm ~/.tmux.conf
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-rm ~/.config/mpv/mpv.conf
-ln -s ~/.dotfiles/mpv.conf ~/.config/mpv/mpv.conf
-rm -rf ~/.config/alacritty/
-mkdir ~/.config/alacritty/
-ln -s ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
-rm ~/.config/starship.toml
-ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
+stow --adopt .
+
 
 echo DONE!
--- lvim/config.lua
+++
@@ -1,6 +0,0 @@
-reload "user.pyright"
-reload "user.clangd"
-
-
-lvim.keys.normal_mode["<Tab>"] = ":bnext<cr>"
-lvim.keys.normal_mode["<S-Tab>"] = ":bprev<cr>"
